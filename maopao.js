17a34218d272efb716b39b0b39c5606d261cbe7e
qxl1231 <Qxl278931058>
2019-02-28 17:00:01 +0800

    冒泡排序 优化版

---
+++ maopao.js
@@ -0,0 +1,44 @@
+// const _ = require('lodash');
+// const sort = (arr) => {
+//
+//     let len = arr.length - 1;
+//     for (let i = 0; i < len; i++) {
+//         for (let j = 0; j < len - i; j++) {
+//             if (arr[j] > arr[j + 1]) {
+//                 let tmp = arr[j];
+//                 arr[j] = arr[j + 1];
+//                 arr[j + 1] = tmp;
+//             }
+//         }
+//     }
+//     return arr;
+// };
+// let arr = [5, 7, 2, 9, 3, 8, 4, 7, 1];
+//
+// console.log(sort(arr));
+
+
+//快速排序优化=>基于冒泡+二分查找
+const quickSort = (arr) => {
+    if (arr.length <= 1) {
+        return arr;
+    }
+    let mid = Math.floor(arr.length / 2);
+    let midNum = arr.splice(mid, 1)[0];
+    // let midNum=arr[mid];//这里为什么要用splice?如果用下面这张,会循环递归,死循环报错
+    console.log(midNum);
+    let left = [];
+    let right = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < midNum) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
+        }
+    }
+    return quickSort(left).concat(midNum, quickSort(right))
+
+};
+let arr = [5, 7, 2, 9, 3, 8, 4, 17, 1, 11];
+
+console.log(quickSort(arr));
